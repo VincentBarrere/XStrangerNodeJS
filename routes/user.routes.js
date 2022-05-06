829e26d887f61dbdf5edd4563d800358c27869fe
vincentbarrere <vincentbarrere0@gmail.com>
2022-05-06 15:29:10 +0200

    start UserController and Get function User

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
 router.post("/register", authController.signUp);
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
 
 module.exports = router;
