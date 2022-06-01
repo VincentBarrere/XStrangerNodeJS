86e6aae946e8c07f89f6da207cde24a0dbe3a3fd
VincentBarrere <vincentbarrere0@gmail.com>
2022-06-01 12:49:41 +0200

    init jwtAuthentication

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,10 @@ const userController = require("../controllers/user.controller");
 
 //auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
-//user display
+//user crud
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
 router.put("/:id", userController.updateUser);
