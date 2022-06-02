cada406d4af409bf7b5467116360e666ab75f36c
VincentBarrere <vincentbarrere0@gmail.com>
2022-06-02 12:54:09 +0200

    feat jwtAuthenticator

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       validate: { validator: isEmail },
       lowercase: true,
+      unique: true,
       trim: true,
     },
     password: {
