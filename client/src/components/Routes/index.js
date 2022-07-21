bfc94eca8bea7cc877de23acff5338fcb6327bb5
VincentBarrere <vincentbarrere0@gmail.com>
2022-07-21 18:26:22 +0200

    featLog

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
+import Navbar from "../Navbar";
 
 function index() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profil" element={<Profil />} />
-      <Route path="/trending" element={<Trending />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/trending" element={<Trending />} />
+      </Routes>
+      <Navigate to="/" replace={true} />
+    </>
   );
 }
 
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -29,9 +29,7 @@ module.exports.signInErrors = (err) => {
 
   if (err.message.includes("email")) {
     errors.email = "Email inconnu";
-  }
-
-  if (err.message.includes("password")) {
+  } else if (err.message.includes("password")) {
     errors.password = "Mauvais mot de passe";
   }
   return errors;
